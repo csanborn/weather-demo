e7e266042d550ae9a0e3bcfb235dd173f9dd3eea
Chris Sanborn <granimalcracker@gmail.com>
2021-05-31 19:14:56 -0700

    Add test to ensure <App/> renders the search screen

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+import { unmountComponentAtNode } from "react-dom";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+  jest.useFakeTimers();
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+  jest.useRealTimers();
+});
+
+
+test('renders search screen', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/City Name/i);
+  expect(textElement).toBeInTheDocument();
 });
